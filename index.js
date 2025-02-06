8936b9426d0a8a81fadc49bb823c1f11c936570c
Enk0-A <sergiyenkoyan@gmail.com>
2025-02-05 20:43:24 -0600

    Starter files, module, lite code

---
+++ index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer"
+
+// TODO: Create an array of questions for user input
+
+const questions = ["What is the title of your project?", "What was your motivation for the project?", "Why did you build this project?", ];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
