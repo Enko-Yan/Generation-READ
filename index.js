dea98251f45943777a117817463f8256ec8271fb
Enk0-A <sergiyenkoyan@gmail.com>
2025-02-07 23:40:38 -0600

    Array questions complete, function to write readme complete

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import inquirer from "inquirer"
+import fs from "fs"
 
 // TODO: Create an array of questions for user input
 
-const questions = ["What is the title of your project?", "What was your motivation for the project?", "Why did you build this project?", ];
+const questions = ["What is the title of your project?", "What was your motivation for the project?", "What does your project do?",
+"What technologies were used?", "How do you install your project?", "How do you use your project?", "Credits?", "Choose a license." 
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README', data, err => {
+        err ? console.log(err) : console.log('File written successfully!');
+    })
+};
 
 // TODO: Create a function to initialize app
 function init() {}
