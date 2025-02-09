ffe35c45dcdf95c1f8e58d6edfe164a2e393420b
Enk0-A <sergiyenkoyan@gmail.com>
2025-02-08 22:33:12 -0600

    Spacing in README.md, npm start for package.json, badge for license

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
+export function renderLicenseBadge(license) {
   
+  let renderBadge = " "
+
+  if (license == 'MIT') {
+    renderBadge = `![Static Badge](https://img.shields.io/badge/License-MIT-purple)`
+  } else if (license == 'Apache 2.0') {
+    renderBadge = `![Static Badge](https://img.shields.io/badge/License-Apache%202.0-purple)`
+  } else if (license == `GPL 3.0`) {
+    renderBadge = `![Static Badge](https://img.shields.io/badge/License-GPL%203.0-purple)`
+  } 
+
+  return renderBadge;
 }
 
 // TODO: Create a function that returns the license link
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import inquirer from "inquirer"
 import fs from "fs"
+import { renderLicenseBadge } from "./generateMarkdown.js";
 
-
-// TODO: Create an array of questions for user input
 const questions = [
     { type: 'input', name: 'title', message: "What is the title of your project?" }, 
     { type: 'input', name: 'motivation', message: "What was your motivation for the project?" }, 
@@ -14,43 +13,41 @@ const questions = [
     { type: 'list', name: 'license', message: "Choose a license.", choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'None']},  
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile('README.md', data, err => {
         err ? console.log(err) : console.log('README.md complete!');
     })
 };
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then(answers => {
+        const licenseBadge = renderLicenseBadge(answers.license);
         const readmeContent = `
-        # ${answers.title}
+# ${answers.title}
         
-        ## Description
-        ${answers.description}
+## Description
+${answers.description}
         
-        ## Motivation
-        ${answers.motivation}
+## Motivation
+${answers.motivation}
         
-        ## Technologies used
-        ${answers.technologies}
+## Technologies used
+${answers.technologies}
         
-        ## Installation
-        ${answers.installation}
+## Installation
+${answers.installation}
         
-        ## How to
-        ${answers.usage}
+## How to
+${answers.usage}
         
-        ## Credits
-        ${answers.credits}
+## Credits
+${answers.credits}
         
-        ## License
-        ${answers.license}`;
+## License
+${licenseBadge}`;
 
             writeToFile('README.md', readmeContent);
     });
 }
 
-// Function call to initialize app
 init();
